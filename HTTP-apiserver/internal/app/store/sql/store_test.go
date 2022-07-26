b39b34db0dd40174ec0e0cf2159b63547b2184e9
n1ghtwell <90438690+n1ghtwell@users.noreply.github.com>
2022-07-26 03:10:23 +0300

    Add files via upload

---
+++ HTTP-apiserver/internal/app/store/sql/store_test.go
@@ -0,0 +1,19 @@
+package sql_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("databaseurl")
+	if databaseURL == "" {
+		databaseURL = "host=localhost dbname=restapi_test sslmode=disable"
+	}
+
+	os.Exit(m.Run())
+}
